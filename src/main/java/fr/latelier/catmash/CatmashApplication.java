9d6a45f53f28363329fb8fb2106b3836b33e9fa7
bikach <chakibhoud.io@gmail.com>
2018-11-12 11:10:08 +0100

    first commit

---
+++ src/main/java/fr/latelier/catmash/CatmashApplication.java
@@ -0,0 +1,12 @@
+package fr.latelier.catmash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatmashApplication.class, args);
+    }
+}
