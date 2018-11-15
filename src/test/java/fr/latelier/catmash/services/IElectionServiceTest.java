67729c158a037818e717cf4ec42c426c2c57518e
bikach <chakibhoud.io@gmail.com>
2018-11-15 11:37:13 +0100

    DTOCast implemente avec test unitaire

---
+++ src/test/java/fr/latelier/catmash/services/IElectionServiceTest.java
@@ -0,0 +1,15 @@
+package fr.latelier.catmash.services;
+
+import fr.latelier.catmash.entities.Candidate;
+import fr.latelier.catmash.services.implement.ElectionServiceImp;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class IElectionServiceTest {
+
+
+
+}
